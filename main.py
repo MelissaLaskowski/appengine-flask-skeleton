f1d8771e5e9c944dd61495c10b6bf90fc8c37966
Melissa Laskowski <melissalaskowski@melissas-mbp.wireless.rit.edu>
2018-11-19 17:35:08 -0500

    add to render

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """`main` is the top level module for your Flask application."""
 
 # Import the Flask Framework
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
@@ -9,9 +9,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    """Return a friendly HTTP greeting."""
-    return 'Hello World!'
-
+	return render_template("landing.html")
 
 @app.errorhandler(404)
 def page_not_found(e):
